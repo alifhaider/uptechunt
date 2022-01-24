be1d794ca503d8ef214ad20a50fff751ac3c8b1d
Alif Haider <alifhaider57@gmail.com>
2022-01-24 14:05:26 +0600

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../styles/components/Footer.module.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return <div></div>;
 };
 
 export default Footer;
